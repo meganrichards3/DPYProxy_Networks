70e2aee99d5fa0d9ae7cbe86e024c66f16dd9108
Megan Richards <mr7401@nyu.edu>
2025-05-09 23:46:58 -0400

    test loop over TLS and TCP frag sizes

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 N=50  # Define the number of tests to run for each website
-frag_size=5
 file_path=controlled_website.txt #citizen_lab_censored_50.txt
-# Run the tests for dpyproxy 
-for ((i=1; i<=N; i++)); do
-    while IFS= read -r website; do
-        echo "Running test $i for website: $website"
-        python test.py \
-            --website $website  \
-            --setting dpyproxy \
-            --tcp_frag  \
-            --frag_size $frag_size
-        sleep 10
-    done < $file_path
-done
+# Define an array of frag sizes to iterate over
+frag_sizes=(2 5 10 15 20 40 60 80 100)
 
-sleep 100
+# Run the tests for dpyproxy with different frag sizes
+for frag_size in "${frag_sizes[@]}"; do
+    for ((i=1; i<=N; i++)); do
+    
+        while IFS= read -r website; do
+            echo "Running test $i / $N for website: $website with frag_size: $frag_size"
+            # TCP Fragmentation Only!
+            python test.py \
+                --website $website  \
+                --setting dpyproxy \
+                --tcp_frag  \
+                --no-record_frag \ 
+                --frag_size $frag_size
+            sleep 10
+            # TLS Fragmentation Only!
+            python test.py \
+                --website $website  \
+                --setting dpyproxy \
+                --record_frag  \
+                --no-tcp_frag \
+                --frag_size $frag_size
+            sleep 10
+            # TCP and TLS Fragmentation
+            python test.py \
+                --website $website  \
+                --setting dpyproxy \
+                --tcp_frag  \
+                --record_frag \
+                --frag_size $frag_size 
+        done < $file_path
+    done
+    sleep 100
+done
 
 # Run tests for proxy baseline
 for ((i=1; i<=N; i++)); do
     while IFS= read -r website; do
-        echo "Running test $i for website: $website"
+        echo "Running proxy baseline test $i / $N for website: $website"
         python test.py \
             --website $website  \
             --setting proxy_baseline 
@@ -29,10 +50,11 @@ for ((i=1; i<=N; i++)); do
 done
 
 sleep 100
+
 # Run tests for baseline
 for ((i=1; i<=N; i++)); do
     while IFS= read -r website; do
-        echo "Running test $i for website: $website"
+        echo "Running baseline test $i for website: $website"
         python test.py \
             --website $website \
             --setting baseline 
